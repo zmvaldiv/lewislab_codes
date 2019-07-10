75be096af9b1bca9ccd7860ddb4fb4419d0a5a44
Zenia Valdiviezo <zenia@dhcp-wifi-8021x-155-41-90-213.bu.edu>
2019-07-10 16:41:10 -0400

    the peaks of the ECG are now plotted on the same plot; plotted the BCG peak (and near by peaks) of an interval; both plots made pretty with new markers and colors

--- bcg_project/practicescript2.m
+++ bcg_project/practicescript2.m
@@ -1,18 +1,26 @@
+%% ECG Peaks and Plot
 %Trying to figure out how to ID the BCG artifact with find peaks
+t = timeEEG;        % Time Vector                                          
+[Rpks,Rlocs] = findpeaks(ECG,'MinPeakHeight',1e3,'MinPeakDistance',150);
+Rtime = t(Rlocs);
 figure()
-findpeaks(-1*ECG,'MinPeakHeight',1e3,'MinPeakDistance',150);
-%Use ECG peaks to find the EEG BCG artifact  
-
-
-t = timeEEG;                                             % Time Vector
+plot(timeEEG,ECG,'r')            
+hold on
+plot(Rtime, Rpks, 'vb','MarkerFaceColor','b')
+hold off
+%findpeaks(ECG,'MinPeakHeight',1e3,'MinPeakDistance',150);
+title('ECG with R Wave Peaks Marked');
+    %Use ECG peaks to find the EEG BCG artifact  
+%% EEG Peaks and Plot
 y = EEG125;                                              % Signal
 t_int = [819.7, 820];                                    % Time Interval
 idx = find((t >= t_int(1)) & (t <= t_int(2)));           % Indices Corresponding To Time Interval
 [pks,locs] = findpeaks(y(idx),'MinPeakHeight',20);       % Find Peaks In Time Interval
 adjlocs = locs + idx(1)-1;                               % Adjust ?locs? To Correct For Offset
-figure
+figure()
 plot(t, y)
 hold on
-plot(t(adjlocs), pks, '^r')
+plot(t(adjlocs), pks, 'vr','MarkerFaceColor','r')
 hold off
 grid
+title('EEG with BCG Artifact Marked');
