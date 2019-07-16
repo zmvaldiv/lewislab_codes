2b9b1514a975715352056f2601b37396c21cbbd1
Zenia Valdiviezo <zenia@dhcp-wifi-8021x-155-41-79-218.bu.edu>
2019-07-16 11:22:32 -0400

    ecg peaks are now marked on ecg plot

--- bcg_project/ecgcomboplot.m
+++ bcg_project/ecgcomboplot.m
@@ -1,6 +1,13 @@
 %Plots the EEG data with the ECG data in the same figure, but different
 %axis
-function ecgcomboplot(ECGdata,EEGdata,channum,timeEEG)
+function ecgcomboplot(ECGdata,EEGdata,channum,timeEEG,rpeaks)
+rpks = ones(size(rpeaks));
+yval = rpks*2000;
+for i=1:length(rpeaks)
+    rpks(i) = rpeaks(i).latency/200;
+end
+
+
 figure()
 ax1 = subplot(2,1,1);
 plot(timeEEG,EEGdata,'b')
@@ -8,6 +15,11 @@ title(['EEG Channel ',num2str(channum)])
 
 ax2 = subplot(2,1,2);
 plot(timeEEG,ECGdata,'r')            
-title('ECG')  
+hold on
+scatter(rpks,yval)
+hold off
+title('ECG') 
+
+sgtitle('ECG and EEG with Peaks Marked')
 linkaxes([ax1,ax2],'x');
 end
