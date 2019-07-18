3424f5763701a64c9c22a05f26bf8217f73710f6
Zenia Valdiviezo <zmvaldiv@bu.edu>
2019-07-18 18:09:32 -0400

    rpeaks now plotted on eeg data for easier ID-ing of bcg artifact

--- bcg_project/ecgcomboplot.m
+++ bcg_project/ecgcomboplot.m
@@ -7,12 +7,15 @@ yval = ones(size(rpks))*2000;
 figure()
 ax1 = subplot(2,1,1);
 plot(timeEEG,EEGdata,'b')
+hold on
+scatter(rpks,yval/100,'MarkerEdgeColor',[0.9290 0.6940 0.1250]);
+hold off
 title(['EEG Channel ',num2str(channum)])
 
 ax2 = subplot(2,1,2);
 plot(timeEEG,ECGdata,'r')            
 hold on
-scatter(rpks,yval)
+scatter(rpks,yval,'MarkerEdgeColor',[0.9290 0.6940 0.1250]);
 hold off
 title('ECG') 
 
