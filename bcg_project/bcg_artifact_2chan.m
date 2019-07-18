9f3c5d694487fa9ce67e96d52061dfb00106a015
Zenia Valdiviezo <zmvaldiv@bu.edu>
2019-07-18 18:08:34 -0400

    eeg data is now stored in a struc; added c125 plot

--- bcg_project/bcg_artifact_2chan.m
+++ bcg_project/bcg_artifact_2chan.m
@@ -24,12 +24,13 @@ channelpath = [mainpath,'/eeg/fs200_averef/', run];
 rawECG = loadchan(channelpath,'ecg');
 ECG = -1*rawECG;    %multiply the ECG by -1 to flip it for easier reading of the R wave and R interval
 
-EEG125 = loadchan(channelpath,125);    %channel type is bcg
-EEG126 = loadchan(channelpath,126);    %channel type is eeg
-EEG31 = loadchan(channelpath,31);      %channel type is eeg
-EEG26 = loadchan(channelpath,26);      %channel type is bcg
-EEG241 = loadchan(channelpath,241);    %channel type is cheek
-EEG238 = loadchan(channelpath,238);    %channel type is cheek
+EEGdata = struct;
+EEGdata.c125 = loadchan(channelpath,125);    %channel type is bcg
+EEGdata.c126 = loadchan(channelpath,126);    %channel type is eeg
+EEGdata.c31 = loadchan(channelpath,31);      %channel type is eeg
+EEGdata.c26 = loadchan(channelpath,26);      %channel type is bcg
+EEGdata.c241 = loadchan(channelpath,241);    %channel type is cheek
+EEGdata.c238 = loadchan(channelpath,238);    %channel type is cheek
 
 hdr = loadchan(channelpath,'hdr');
 timeEEG = hdr.ts;
@@ -43,16 +44,17 @@ rpks = rpks(1:length(rpks)-1);
 
 %% Plot EEG and ECG data
 %Plots data in one figure but two different axis
+ecgcomboplot(ECG,EEGdata.c125,125,timeEEG,rpks);
 
-ecgcomboplot(ECG,EEG126,126,timeEEG,rpks);
+ecgcomboplot(ECG,EEGdata.c126,126,timeEEG,rpks);
 
-ecgcomboplot(ECG,EEG31,31,timeEEG,rpks);
+ecgcomboplot(ECG,EEGdata.c31,31,timeEEG,rpks);
 
-ecgcomboplot(ECG,EEG26,26,timeEEG,rpks);
+ecgcomboplot(ECG,EEGdata.c26,26,timeEEG,rpks);
 
-ecgcomboplot(ECG,EEG241,241,timeEEG,rpks);
+ecgcomboplot(ECG,EEGdata.c241,241,timeEEG,rpks);
 
-ecgcomboplot(ECG,EEG238,238,timeEEG,rpks);
+ecgcomboplot(ECG,EEGdata.c238,238,timeEEG,rpks);
 
 %% Finding the BCG artifact
 
