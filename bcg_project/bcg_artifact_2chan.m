7523f961e24aeb8055144ca5b651dff83310aeba
Zenia Valdiviezo <zmvaldiv@bu.edu>
2019-08-21 14:21:12 -0400

    data loads into matrix; mean is calc; mean plotted

--- bcg_project/bcg_artifact_2chan.m
+++ bcg_project/bcg_artifact_2chan.m
@@ -1,4 +1,5 @@
- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Heart Brain Interaction EEG Artifact
 % By: Zenia
 % Description: Identifies the BCG artifact in EEG data and then finds the
@@ -20,17 +21,43 @@ channelpath = [mainpath,'/eeg/fs200_averef/', run];
 % channelpath = [mainpath,'/eeg/fs200/', run]; 
 
 %% Load EEG data
+frontal(1,:)=loadchan(channelpath,31); %frontal region, eeg type chan
+frontal(2,:)=loadchan(channelpath,32);
+frontal(3,:)=loadchan(channelpath,26);
+frontal(4,:)=loadchan(channelpath,25);
 
+occip(1,:)=loadchan(channelpath,116); %back/occipital region, eeg type chan
+occip(2,:)=loadchan(channelpath,126);
+occip(3,:)=loadchan(channelpath,136);
+occip(4,:)=loadchan(channelpath,149);
+
+%BCG
+Lfront(1,:)=loadchan(channelpath,241);   %left frontal region
+Lfront(2,:)=loadchan(channelpath,244);
+Lfront(3,:)=loadchan(channelpath,242);
+
+Rfront(1,:)=loadchan(channelpath,238); % right frontal region
+Rfront(2,:)=loadchan(channelpath,234);
+Rfront(3,:)=loadchan(channelpath,239);
+
+Lmid(1,:)=loadchan(channelpath,256);  %left mid region
+Lmid(2,:)=loadchan(channelpath,82);
+
+Rmid(1,:)=loadchan(channelpath,229);  % right mid region
+Rmid(2,:)=loadchan(channelpath,217);
+
+occip_b(1,:)=loadchan(channelpath,146); %back/occipital region
+occip_b(2,:)=loadchan(channelpath,147);
+occip_b(3,:)=loadchan(channelpath,156);
+
+frontal_b(1,:)=loadchan(channelpath,27); % frontal region
+frontal_b(1,:)=loadchan(channelpath,20);
+frontal_b(1,:)=loadchan(channelpath,22);
+frontal_b(1,:)=loadchan(channelpath,14);
+
+%% Load in extra data
 rawECG = loadchan(channelpath,'ecg');
 ECG = -1*rawECG;    %multiply the ECG by -1 to flip it for easier reading of the R wave and R interval
-
-EEGdata = struct;
-EEGdata.c125 = loadchan(channelpath,125);    %channel type is bcg
-EEGdata.c126 = loadchan(channelpath,126);    %channel type is eeg
-EEGdata.c31 = loadchan(channelpath,31);      %channel type is eeg
-EEGdata.c26 = loadchan(channelpath,26);      %channel type is bcg
-EEGdata.c241 = loadchan(channelpath,241);    %channel type is cheek
-EEGdata.c238 = loadchan(channelpath,238);    %channel type is cheek
 
 hdr = loadchan(channelpath,'hdr');
 timeEEG = hdr.ts;
@@ -56,6 +83,59 @@ ecgcomboplot(ECG,EEGdata.c241,241,timeEEG,rpks);
 
 ecgcomboplot(ECG,EEGdata.c238,238,timeEEG,rpks);
 
-%% Finding the BCG artifact
+%% Finding the average BCG artifact
 
+mean_frontal = mean(frontal);
+mean_occip = mean(occip);
+mean_Lfront = mean(Lfront);
+mean_Rfront = mean(Rfront);
+mean_Lmid = mean(Lmid);
+mean_Rmid = mean(Rmid);
+mean_occip_b = mean(occip_b);
+mean_frontal_b = mean(frontal_b);
 
+%% Plot mean
+figure()
+plot(timeEEG,mean_frontal)
+title('Frontal (EEG)')
+
+figure()
+plot(timeEEG,mean_occip)
+title('Occipital (EEG)')
+
+figure()
+plot(timeEEG,mean_Lfront)
+title('Left Frontal (BCG)')
+
+figure()
+plot(timeEEG,mean_Rfront)
+title('Right Frontal (BCG)')
+
+figure()
+plot(timeEEG,mean_Lmid)
+title('Left Mid (BCG)')
+
+figure()
+plot(timeEEG,mean_Rmid)
+title('Right Mid (BCG)')
+
+figure()
+plot(timeEEG,mean_occip_b)
+title('Occipital (BCG)')
+
+figure()
+plot(timeEEG,mean_frontal_b)
+title('Frontal (BCG)')
+
+% ax = cell(1,length(channel));
+% j = 1;
+% for i = 1:length(channel)-2
+%    ax{i} = subplot(6,2,j);
+%    plot(timeEEG,channel{i,3})
+%    ylabel(channel{i,1}) 
+%    j = j+2;
+% end
+% 
+% title(ax{1},'EEG Means')
+% xlabel(ax{6},'time (sec)')
+% linkaxes([ax{:},axspec],'x');
