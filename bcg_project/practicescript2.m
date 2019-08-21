2bfddcf7e77b1251fa73316b0c433b9ac3c0af95
Zenia Valdiviezo <zmvaldiv@bu.edu>
2019-08-21 14:24:11 -0400

    thinking space; have done more thinking

--- bcg_project/practicescript2.m
+++ bcg_project/practicescript2.m
@@ -1,10 +1,10 @@
 %% ECG Peaks and Plot
 %Trying to figure out how to ID the BCG artifact with find peaks
-t = timeEEG;        % Time Vector                                          
-[Rpks,Rlocs] = findpeaks(ECG,'MinPeakHeight',1e3,'MinPeakDistance',150);
+t = EEGdata.base.hdr.ts;        % Time Vector                                          
+[Rpks,Rlocs] = findpeaks(-1*EEGdata.base.ecg,'MinPeakHeight',1e3);
 Rtime = t(Rlocs);
 figure()
-plot(timeEEG,ECG,'r')            
+plot(EEGdata.base.hdr.ts,-1*EEGdata.base.ecg,'r')            
 hold on
 plot(Rtime, Rpks, 'vb','MarkerFaceColor','b')
 hold off
@@ -53,19 +53,20 @@ hold off
 % secondpath = ['/eeg/fs200_averef/run0', run]; 
 % 
 % folder = [firstpath,secondpath];
+subjectsA = 1:25;
 subjectsB = [2:10,15,17,19,20,21,22,24];
 j = 1;
-for i=subjectsB
+for i=subjectsA
     subID = num2str(i);
     
-    firstpath = ['/Volumes/Research/eng_research_lewislab/data/osceeg_frommgh/osceeg',subID,'b'];
+    firstpath = ['/Volumes/Research/eng_research_lewislab/data/osceeg_frommgh/osceeg',subID,'a'];
     secondpath = '/eeg/fs200_averef/run01'; 
     folder = [firstpath,secondpath,'/rpeaks.mat'];
-    present = exist(folder);
-    
+    %present = exist(folder);
+    present = exist([firstpath,'/behav'])
     
     if present>0
-        pks_existrun1(j) = str2num(subID);
+        clicks_exist(j) = str2num(subID);
         value(j) = present;
         j = j+1;
     end
@@ -103,7 +104,7 @@ end
 %%
 yval = ones(size(rpks))*30;
 figure();
-plot(timeEEG, EEGdata.c125)
+plot(timeEEG, EEGdata.c31)
 hold on
 for i=1:length(rpks)
     plot(timeEEG(adjlocs{i}), pks{i}, 'vr','MarkerFaceColor','r')
@@ -111,10 +112,10 @@ end
 scatter(rpks,yval)
 hold off
 grid
-title('EEG125 with BCG Artifact Marked');
+title('EEG31 with BCG Artifact Marked');
 %% 
 yval = ones(size(rpks))*30;
-plot(timeEEG, EEGdata.c238)
+plot(timeEEG, EEGdata.c31)
 hold on
 for i=1:length(rpks)
     plot(timeEEG(adjlocs_238{i}), pks_238{i}, 'vr','MarkerFaceColor','r')
@@ -125,7 +126,7 @@ grid
 title('EEG238 with BCG Artifact Marked');
 
 %%
-EEGdata=struct;
+
 EEGdata.c125=EEG125;
 EEGdata.c126=EEG126;
 EEGdata.c238=EEG238;
@@ -133,3 +134,42 @@ EEGdata.c241=EEG241;
 EEGdata.c26=EEG26;
 EEGdata.c31=EEG31;
 %%
+%EEG
+EEGdata.c31=loadchan(channelpath,31); %frontal region, eeg type chan
+EEGdata.c32=loadchan(channelpath,32);
+EEGdata.c26=loadchan(channelpath,26);
+EEGdata.c25=loadchan(channelpath,25);
+
+%EEG
+EEGdata.c116=loadchan(channelpath,116); %back/occipital region, eeg type chan
+EEGdata.c126=loadchan(channelpath,126);
+EEGdata.c136=loadchan(channelpath,136);
+EEGdata.c149=loadchan(channelpath,149);
+
+%BCG
+EEGdata.c241=loadchan(channelpath,241);   %left frontal region
+EEGdata.c244=loadchan(channelpath,244);
+EEGdata.c242=loadchan(channelpath,242);
+
+EEGdata.c238=loadchan(channelpath,238); % right frontal region
+EEGdata.c234=loadchan(channelpath,234);
+EEGdata.c239=loadchan(channelpath,239);
+
+EEGdata.c256=loadchan(channelpath,256);  %left mid region
+EEGdata.c82=loadchan(channelpath,82);
+
+EEGdata.c229=loadchan(channelpath,229);  % right mid region
+EEGdata.c217=loadchan(channelpath,217);
+
+EEGdata.c146=loadchan(channelpath,146); %back/occipital region
+EEGdata.c147=loadchan(channelpath,147);
+EEGdata.c156=loadchan(channelpath,156);
+
+EEGdata.c27=loadchan(channelpath,27); % frontal region
+EEGdata.c20=loadchan(channelpath,20);
+EEGdata.c22=loadchan(channelpath,22);
+EEGdata.c14=loadchan(channelpath,14);
+
+%% Plot the mean and standard devation 
+
+
